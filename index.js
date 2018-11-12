5adbe92e7838dcef1b4996be16beb98138a949bf
Lee Len Formento <leebatef@gmail.com>
2018-11-12 14:59:05 -0500

    GET veggies

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ server.get('/', (req, res) => {
     res.json('Welcome to the farm.')
 })
 
+server.get('/veggies', (req, res) => {
+    db('veggies')
+    .then(veggie => {
+        console.log(veggie)
+        res.status(200).json(veggie);
+    })
+    .catch(err => {
+        res.status(500).json(err)
+      });
+})
+
 const port = 8888;
 server.listen(port, function() {
     console.log(`\n Planting at port ${port} \n`);
